1481ecccd21302efea76f6657ceddaf8a5be0588
Laura Alarcon <laura.alarcon@durango.gob.mx>
2024-01-29 15:16:59 -0600

    Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,6 +13,9 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
     AppRoutingModule,
     SharedModule,
   ],
---
+++ src/environments/environments.ts
@@ -0,0 +1,4 @@
+// Declarar variables de entorno
+export const environments = {
+  baseUrl: 'http://localhost:3000'
+}
